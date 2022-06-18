ca54073878371644e608982f38da0e74f7357a85
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-18 21:22:30 +0200

    💄Created DOM of home page

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import "./style/screen.scss";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return <Router></Router>;
+	return (
+		<main>
+			<div className="p-home">
+				<div className="left-side"></div>
+				<div className="right-side">
+					<h1 className="title">Milan Mattheeuws</h1>
+					<h2 className="subtitle">Student Howest Kortrijk</h2>
+					<p className="text">
+						Hello there. I am Milan Mattheeuws, a student in
+						<br />
+						MCT next web development in Kortrijk. I mainly
+						<br />
+						do Frontend and designing.
+					</p>
+					<div className="buttons">
+						<button id="resume" name="resume" className="resume">
+							Resume
+						</button>
+						<button id="about" name="about" className="about">
+							About me
+						</button>
+					</div>
+				</div>
+				<div className="picture"></div>
+				<footer></footer>
+			</div>
+		</main>
+	);
 }
 
 export default App;
