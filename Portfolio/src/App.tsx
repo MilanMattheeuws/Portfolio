236b0daa197f77950726a8f18d53f44ea3afc36a
Milan Mattheeuws <milan.mattheeuws@student.howest.be>
2022-06-18 19:03:09 +0200

    💄 Added SCSS structure

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "./style/screen.scss";
+
 function App() {
 	const [count, setCount] = useState(0);
 
